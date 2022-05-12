9e743d827b60f3e6d0f19ad066c49a689829a541
Avery Harnish <averyharnish@gmail.com>
2022-05-11 19:47:17 -0500

    feat: pure CSS redesign (#39)
    say goodbye to the sidebar, bootstrap, and my profile picture. now we have emoji cycles and our own custom CSS, isn't it grand? definitely the end of an era, there are now no traces of the old bootstrap resume theme i started out with.

---
+++ public/js/index.js
@@ -0,0 +1,34 @@
+const emojis = [
+  "😁",
+  "🤔",
+  "🤯",
+  "🙄",
+  "🤩",
+  "😏",
+  "😎",
+  "😷",
+  "🙃",
+  "😐",
+  "😌",
+];
+let emojiIndex = 0;
+
+let emojiHandle;
+
+const getEmojiHandle = () => {
+  if (!emojiHandle) {
+    emojiHandle = document.getElementById("emoji");
+  }
+  return emojiHandle;
+};
+
+setInterval(() => {
+  const emojiHandle = getEmojiHandle();
+  emojiHandle.innerHTML = "";
+  emojiHandle.innerHTML = emojis[emojiIndex];
+  if (emojiIndex == emojis.length - 1) {
+    emojiIndex = 0;
+  } else {
+    emojiIndex += 1;
+  }
+}, 600);
