d23d4c640d4f07b9b719abbbebb3e29a3130079e
Avery Harnish <averyharnish@gmail.com>
2022-05-14 15:41:38 -0500

    feat: create github comment on non-main branches

--- lighthouse/src/generate.js
+++ lighthouse/src/generate.js
@@ -2,36 +2,61 @@ const path = require("path");
 const fs = require("fs");
 const { makeBadge } = require("badge-maker");
 const { LIGHTHOUSE_DIR, displayFile, info } = require("./utils.js");
-const {
-  updateScoresJSONFromReport,
-} = require("./scores.js");
+const { updateScoresJSONFromReport } = require("./scores.js");
 
-const BADGES_DIR = path.join(LIGHTHOUSE_DIR, "badges");
-try {
-  fs.mkdirSync(BADGES_DIR);
-} catch (e) {
-  info(`this is probably fine: ${e}`);
-}
-const scores = updateScoresJSONFromReport();
+const generateBadges = () => {
+  const scores = updateScoresJSONFromReport();
+  const badges = [];
+  for (const [camelCaseName, score] of Object.entries(scores)) {
+    const name = camelCaseName.replace(/([A-Z])/g, " $1").toLowerCase();
 
-for (const [camelCaseName, score] of Object.entries(scores)) {
-  const name = camelCaseName.replace(/([A-Z])/g, " $1").toLowerCase();
-  const badgePath = path.join(BADGES_DIR, `${camelCaseName}.svg`);
-  let color = "red";
-  if (score >= 90) {
-    color = "success";
-  } else if (score >= 50) {
-    color = "yellow";
+    let color = "red";
+    if (score >= 90) {
+      color = "success";
+    } else if (score >= 50) {
+      color = "yellow";
+    }
+    const svg = makeBadge({
+      label: name,
+      message: `${score}%`,
+      labelColor: "gray",
+      color,
+    });
+    badges.push({ camelCaseName, name, score, svg, color });
   }
-  const badgeContents = makeBadge({
-    label: name,
-    message: `${score}%`,
-    labelColor: "gray",
-    color,
-  });
-  info(`creating '${name}' badge with a score of '${score}'...`);
-  fs.writeFileSync(badgePath, badgeContents);
-  info(`created ${displayFile(badgePath)} ✅`);
-}
+  return badges;
+};
 
-info(`created all lighthouse badges! 🎈🎉🥳`);
+const generateComment = () => {
+  const badges = generateBadges();
+  let comment = "# Lighthouse Audit Results\n\n";
+  comment += "| test | score |\n| --- | --- |\n";
+  for (const { name, score } of badges) {
+    comment += `| ${name} | ${score}% |\n`;
+  }
+  info("printing comment for GitHub PR...");
+  process.stdout.write(comment);
+};
+
+const generateAndWriteBadges = () => {
+  const badges = generateBadges();
+  const BADGES_DIR = path.join(LIGHTHOUSE_DIR, "badges");
+  try {
+    fs.mkdirSync(BADGES_DIR);
+  } catch (e) {
+    info(`this is probably fine: ${e}`);
+  }
+  for (const { camelCaseName, name, score, svg } of badges) {
+    const badgePath = path.join(BADGES_DIR, `${camelCaseName}.svg`);
+    info(`creating '${name}' badge with a score of '${score}'...`);
+    fs.writeFileSync(badgePath, svg);
+    info(`created ${displayFile(badgePath)} ✅`);
+  }
+
+  info(`created all lighthouse badges! 🎈🎉🥳`);
+};
+
+module.exports = {
+  generateAndWriteBadges,
+  generateComment,
+};
