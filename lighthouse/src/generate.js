6580a77e0aa529d750a27b60c3cb05166765ac64
Avery Harnish <averyharnish@gmail.com>
2022-05-14 14:33:06 -0500

    fix: badge creation

--- lighthouse/src/generate.js
+++ lighthouse/src/generate.js
@@ -2,38 +2,36 @@ const path = require("path");
 const fs = require("fs");
 const { makeBadge } = require("badge-maker");
 const { LIGHTHOUSE_DIR, displayFile, info } = require("./utils.js");
-const { updateScoresJSONFromReport, validateScoresJSON } = require("./scores.js");
+const {
+  updateScoresJSONFromReport,
+} = require("./scores.js");
 
 const BADGES_DIR = path.join(LIGHTHOUSE_DIR, "badges");
 try {
   fs.mkdirSync(BADGES_DIR);
 } catch (e) {
-  info(`this is probably fine: ${e}`)
+  info(`this is probably fine: ${e}`);
 }
-const { changed, scores } = updateScoresJSONFromReport();
+const scores = updateScoresJSONFromReport();
 
-if (changed) {
-  for (const [camelCaseName, score] of Object.entries(scores)) {
-    const name = camelCaseName.replace(/([A-Z])/g, " $1").toLowerCase();
-    const badgePath = path.join(BADGES_DIR, `${camelCaseName}.svg`);
-    let color = "red";
-    if (score >= 90) {
-      color = "success"
-    } else if (score >= 50) {
-      color = "yellow";
-    }
-    const badgeContents = makeBadge({
-      label: name,
-      message: `${score}%`,
-      labelColor: "gray",
-      color,
-    });
-    info(`creating '${name}' badge with a score of '${score}'...`);
-    fs.writeFileSync(badgePath, badgeContents);
-    info(`created ${displayFile(badgePath)} ✅`);
+for (const [camelCaseName, score] of Object.entries(scores)) {
+  const name = camelCaseName.replace(/([A-Z])/g, " $1").toLowerCase();
+  const badgePath = path.join(BADGES_DIR, `${camelCaseName}.svg`);
+  let color = "red";
+  if (score >= 90) {
+    color = "success";
+  } else if (score >= 50) {
+    color = "yellow";
   }
-
-  info(`created all lighthouse badges! 🎈🎉🥳`);
-} else {
-  info("skipping badge generation, because there were no changes! 🎈🎉🥳");
+  const badgeContents = makeBadge({
+    label: name,
+    message: `${score}%`,
+    labelColor: "gray",
+    color,
+  });
+  info(`creating '${name}' badge with a score of '${score}'...`);
+  fs.writeFileSync(badgePath, badgeContents);
+  info(`created ${displayFile(badgePath)} ✅`);
 }
+
+info(`created all lighthouse badges! 🎈🎉🥳`);
